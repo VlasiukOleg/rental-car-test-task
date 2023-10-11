6547c8833319811c8adf7ced2b7b6218553c526b
VlasiukOleg <106608565+VlasiukOleg@users.noreply.github.com>
2023-10-11 07:27:12 +0000

    Deploying to gh-pages from @ VlasiukOleg/rental-car-test-task@d80bfc76fb6b93970796a97e40c54bd9b33d0f3c 🚀

---
+++ static/js/126.9d8aeee1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrental_car_test_task=self.webpackChunkrental_car_test_task||[]).push([[126],{9126:function(t,a,e){e.d(a,{FKi:function(){return n}});var r=e(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"}}]})(t)}}}]);
+//# sourceMappingURL=126.9d8aeee1.chunk.js.map
